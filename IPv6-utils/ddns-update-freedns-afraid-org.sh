901bcb65db6485781f52b4d95c3b00606bcb8fd5
Melan Jayasinghage <melan@apache.org>
2019-04-04 00:54:29 +0530

    updated ddns scrips

--- IPv6-utils/ddns-update-freedns-afraid-org.sh
+++ IPv6-utils/ddns-update-freedns-afraid-org.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 ip6_addr=`./get-addr-dialog-bb.sh eth0` #
-last_ip6_addr=`cat ./.last_ip6_addr`
+last_ip6_addr=`cat ./.last_ip6_addr 2> /dev/null`
 host=`hostname -f`
 auth='<user>:<password>' # freedns.afraid.org credentails
 if [ "$ip6_addr" != "$last_ip6_addr" ] 
 then
-    curl -k -v "http://freedns.afraid.org/nic/update?hostname=$host&myip=$ip6_addr" --user $auth
+    curl -k "https://freedns.afraid.org/nic/update?hostname=$host&myip=$ip6_addr" --user $auth
     echo "$ip6_addr" > ./.last_ip6_addr
 else
     echo "Address $ip6_addr has not changed"    
