c063fb49662309688bfd4821f10f3a8a6b424897
Melan Jayasinghage <melan@apache.org>
2021-08-27 23:31:20 +0530

    Added sample for get timezone and locate in c++

---
+++ misc/locate.cpp
@@ -0,0 +1,11 @@
+#include <locale>
+#include <iostream>
+#include <string>
+ 
+int main()
+{
+    std::locale loc(std::locale(), new std::ctype<char>);
+    std::cout << "The default locale is " << std::locale().name() << '\n'
+              << "The user's locale is " << std::locale("").name() << '\n'
+              << "A nameless locale is " << loc.name() << '\n';
+}
